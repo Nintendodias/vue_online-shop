ddaf4bd7f7a0bf15956d48ed995c1a0726dac2b8
Anastasiya <40817643+Nintendodias@users.noreply.github.com>
2021-01-26 00:12:25 +0300

    Домашнее задание урока 4

--- project/src/data/products.js
+++ project/src/data/products.js
@@ -1,200 +1,240 @@
 export default [
   {
+    id: 1,
     name: 'Радионяня Motorola MBP16',
     price: 3690,
     img: '/img/radio.jpg'
   },
   {
+    id: 2,
     name: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
     price: 5660,
     img: '/img/toothbrush.jpg'
   },
   {
+    id: 3,
     name: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
     price: 12000,
     img: '/img/phone.jpg'
   },
   {
+    id: 4,
     name: 'Электроскейт Razor Cruiser',
     price: 25000,
     img: '/img/board.jpg'
   },
   {
+    id: 5,
     name: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
     price: 15000,
     img: '/img/phone-2.jpg'
   },
   {
+    id: 6,
     name: 'Смартфон Xiaomi Redmi 6/128GB',
     price: 9000,
     img: '/img/phone-3.jpg'
   },
   {
+    id: 7,
     name: 'Электрический дрифт-карт Razor Crazy Cart',
     price: 40000,
     img: '/img/bicycle.jpg'
   },
   {
+    id: 8,
     name: 'Гироскутер Razor Hovertrax 2.0',
     price: 34000,
     img: '/img/wheels.jpg'
   },
   {
+    id: 9,
     name: 'Детский трюковой самокат Razor Grom',
     price: 5660,
     img: '/img/scooter.jpg'
   },
   {
+    id: 10,
     name: 'Наушники AirPods с беспроводным зарядным футляром',
     price: 17000,
     img: '/img/airpods.jpg'
   },
   {
+    id: 11,
     name: 'Радионяня Motorola MBP16',
     price: 3690,
     img: '/img/radio.jpg'
   },
   {
+    id: 12,
     name: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
     price: 5660,
     img: '/img/toothbrush.jpg'
   },
   {
+    id: 13,
     name: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
     price: 12000,
     img: '/img/phone.jpg'
   },
   {
+    id: 14,
     name: 'Электроскейт Razor Cruiser',
     price: 25000,
     img: '/img/board.jpg'
   },
   {
+    id: 15,
     name: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
     price: 15000,
     img: '/img/phone-2.jpg'
   },
   {
+    id: 16,
     name: 'Смартфон Xiaomi Redmi 6/128GB',
     price: 9000,
     img: '/img/phone-3.jpg'
   },
   {
+    id: 17,
     name: 'Электрический дрифт-карт Razor Crazy Cart',
     price: 40000,
     img: '/img/bicycle.jpg'
   },
   {
+    id: 18,
     name: 'Гироскутер Razor Hovertrax 2.0',
     price: 34000,
     img: '/img/wheels.jpg'
   },
   {
+    id: 19,
     name: 'Детский трюковой самокат Razor Grom',
     price: 5660,
     img: '/img/scooter.jpg'
   },
   {
+    id: 2,
     name: 'Наушники AirPods с беспроводным зарядным футляром',
     price: 17000,
     img: '/img/airpods.jpg'
   },
   {
+    id: 21,
     name: 'Радионяня Motorola MBP16',
     price: 3690,
     img: '/img/radio.jpg'
   },
   {
+    id: 22,
     name: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
     price: 5660,
     img: '/img/toothbrush.jpg'
   },
   {
+    id: 23,
     name: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
     price: 12000,
     img: '/img/phone.jpg'
   },
   {
+    id: 24,
     name: 'Электроскейт Razor Cruiser',
     price: 25000,
     img: '/img/board.jpg'
   },
   {
+    id: 25,
     name: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
     price: 15000,
     img: '/img/phone-2.jpg'
   },
   {
+    id: 26,
     name: 'Смартфон Xiaomi Redmi 6/128GB',
     price: 9000,
     img: '/img/phone-3.jpg'
   },
   {
+    id: 27,
     name: 'Электрический дрифт-карт Razor Crazy Cart',
     price: 40000,
     img: '/img/bicycle.jpg'
   },
   {
+    id: 28,
     name: 'Гироскутер Razor Hovertrax 2.0',
     price: 34000,
     img: '/img/wheels.jpg'
   },
   {
+    id: 29,
     name: 'Детский трюковой самокат Razor Grom',
     price: 5660,
     img: '/img/scooter.jpg'
   },
   {
+    id: 30,
     name: 'Наушники AirPods с беспроводным зарядным футляром',
     price: 17000,
     img: '/img/airpods.jpg'
   },
   {
+    id: 31,
     name: 'Радионяня Motorola MBP16',
     price: 3690,
     img: '/img/radio.jpg'
   },
   {
+    id: 32,
     name: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
     price: 5660,
     img: '/img/toothbrush.jpg'
   },
   {
+    id: 33,
     name: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
     price: 12000,
     img: '/img/phone.jpg'
   },
   {
+    id: 34,
     name: 'Электроскейт Razor Cruiser',
     price: 25000,
     img: '/img/board.jpg'
   },
   {
+    id: 35,
     name: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
     price: 15000,
     img: '/img/phone-2.jpg'
   },
   {
+    id: 36,
     name: 'Смартфон Xiaomi Redmi 6/128GB',
     price: 9000,
     img: '/img/phone-3.jpg'
   },
   {
+    id: 37,
     name: 'Электрический дрифт-карт Razor Crazy Cart',
     price: 40000,
     img: '/img/bicycle.jpg'
   },
   {
+    id: 38,
     name: 'Гироскутер Razor Hovertrax 2.0',
     price: 34000,
     img: '/img/wheels.jpg'
   },
   {
+    id: 39,
     name: 'Детский трюковой самокат Razor Grom',
     price: 5660,
     img: '/img/scooter.jpg'
   },
   {
+    id: 40,
     name: 'Наушники AirPods с беспроводным зарядным футляром',
     price: 17000,
     img: '/img/airpods.jpg'
