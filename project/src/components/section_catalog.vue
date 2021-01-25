17e09e353f170b08a565629c058ac338419ff50b
Anastasiya <40817643+Nintendodias@users.noreply.github.com>
2021-01-25 18:22:38 +0300

    create component ProductsList

--- project/src/components/section_catalog.vue
+++ project/src/components/section_catalog.vue
@@ -1,50 +1,8 @@
 <template>
 	<section class="catalog">
-		<ul class="catalog__list">
-			<li class="catalog__item" v-for="(product, index) in products" :key="index">
-				<a class="catalog__pic" href="#">
-					<img :src='product.img' :alt="product.name" />
-				</a>
-
-				<h3 class="catalog__title">
-					<a href="#">
-						{{product.name}}
-					</a>
-				</h3>
-
-				<span class="catalog__price">
-					{{product.price}}₽
-				</span>
-
-				<ul class="colors colors--black">
-					<li class="colors__item">
-						<label class="colors__label">
-							<input
-								class="colors__radio sr-only"
-								type="radio"
-								name="color-1"
-								value="#73B6EA"
-								checked=""
-							/>
-							<span class="colors__value" style="background-color: #73B6EA;"> </span>
-						</label>
-					</li>
-					<li class="colors__item">
-						<label class="colors__label">
-							<input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000" />
-							<span class="colors__value" style="background-color: #8BE000;"> </span>
-						</label>
-					</li>
-					<li class="colors__item">
-						<label class="colors__label">
-							<input class="colors__radio sr-only" type="radio" name="color-1" value="#222" />
-							<span class="colors__value" style="background-color: #222;"> </span>
-						</label>
-					</li>
-				</ul>
-			</li>
-		</ul>
-
+		
+		<ProductList :products="products" />
+		
 		<ul class="catalog__pagination pagination">
 			<li class="pagination__item">
 				<a
@@ -102,9 +60,14 @@
 </template>
 
 <script>
-	import products from "../data/products"
+	import ProductList from "./section_catalog/catalog_list.vue";
+	import products from "../data/products";
+
 	export default {
 		name: 'section_catalog',
+		components: {
+			ProductList
+		},
 		data() {
 			return {
 				products
